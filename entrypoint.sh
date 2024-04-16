303a1f1f90dca11a9aa1bc48d5404d28a9647ef8
Chesney Nyffels <chesney@nyffels.be>
2024-04-16 22:52:54 +0200

    Removed typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -f "/runner/activation-script.sh" ]; then
-  /bin/bash ./runner/activation-script.sh
-  rm ./runner/activation-script.sh
+  /bin/bash /runner/activation-script.sh
+  rm /runner/activation-script.sh
 else
   echo "Activation script not found."
 fi
