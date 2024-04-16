938ad30e76b0bbd5a20a7379e2f805d051220254
Chesney Nyffels <chesney@nyffels.be>
2024-04-16 22:41:30 +0200

    Updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-if test -f "/runner/activation-script.sh"; then
+if [ -f "/runner/activation-script.sh" ]; then
   echo "Activation-script found"
   /bin/bash ./runner/run.sh
   rm ./runner/run.sh
   echo "Activation-script completed"
+else
+  echo "Activation script not found."
 fi
 
 echo "completed"
