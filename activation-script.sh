3f86dbbd3397788610549cbfa799081e7310821d
Chesney Nyffels <chesney@nyffels.be>
2024-04-16 21:59:26 +0200

    Updated scripts

--- activation-script.sh
+++ activation-script.sh
@@ -1,1 +1,4 @@
 #!/bin/sh
+curl -o actions-runner-linux-x64-2.315.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.315.0/actions-runner-linux-x64-2.315.0.tar.gz
+tar xzf ./actions-runner-linux-x64-2.315.0.tar.gz
+./config.sh --url https://github.com/moosj-be --token AHCZGP4GRM4MYPAOFVOYHMTGD3SYA
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,7 @@
-#!/bin/sh
-echo "Hello world"
+#!/bin/bash
+if test -f "./runner/run.sh"; then
+  echo "Activation-script found"
+  /bin/bash ./runner/run.sh
+  rm ./runner/run.sh
+  echo "Activation-script completed"
+fi
