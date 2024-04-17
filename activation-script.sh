5447cbad5aa3b4017b7f6eb7693817c48768a6d8
Chesney Nyffels <chesney@nyffels.be>
2024-04-17 07:55:56 +0200

    Added allowROot logic to script

--- activation-script.sh
+++ activation-script.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 cd /runner
-echo 'Download file'
 curl -s -o actions-runner-linux-x64-2.315.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.315.0/actions-runner-linux-x64-2.315.0.tar.gz
-echo 'unpack file'
 tar xzf ./actions-runner-linux-x64-2.315.0.tar.gz
-echo 'Run script'
-./config.sh --url https://github.com/Nyffels-IT --token AHCZGP2OV2KUFLTNYLCPW6TGD5WMC
+RUNNER_ALLOW_RUNASROOT=true ./config.sh --url https://github.com/Nyffels-IT --token AHCZGP2OV2KUFLTNYLCPW6TGD5WMC
