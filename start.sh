ef824f3e1e5a1cb91f80baf0968db0085d65d393
Chesney Nyffels <chesney@nyffels.be>
2024-04-17 15:49:18 +0200

    Updated start script

--- start.sh
+++ start.sh
@@ -2,12 +2,13 @@
 
 REPO=$REPO
 ACCESS_TOKEN=$TOKEN
+REG_TOKEN=$REG_TOKEN
 
-REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
+# REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN}
+./config.sh --unatended --url https://github.com/${REPO} --token ${REG_TOKEN}
 
 cleanup() {
 echo "Removing runner..."
