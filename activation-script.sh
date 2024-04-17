e815e77e23db9d89ab7c2e0824ba26fc8ffa69d6
Chesney Nyffels <chesney@nyffels.be>
2024-04-17 10:59:01 +0200

    Updated activation script

--- activation-script.sh
+++ activation-script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /runner
-curl -s -o actions-runner-linux-x64-2.315.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.315.0/actions-runner-linux-x64-2.315.0.tar.gz
+curl -o actions-runner-linux-x64-2.315.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.315.0/actions-runner-linux-x64-2.315.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.315.0.tar.gz
-RUNNER_ALLOW_RUNASROOT=true ./config.sh --url https://github.com/moosj-be --token AHCZGP5VE5BZCWZNZP2NV23GD6NZC
+RUNNER_ALLOW_RUNASROOT=true ./config.sh --unattended  --acceptteeeula --url https://github.com/moosj-be --token AHCZGP5VE5BZCWZNZP2NV23GD6NZC
