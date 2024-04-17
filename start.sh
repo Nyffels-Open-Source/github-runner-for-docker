5414a7a6bb63a583a969b09ed43cdd6125bbe0fb
Chesney Nyffels <chesney@nyffels.be>
2024-04-17 21:29:01 +0200

    Updated cconfig

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-REPO=$REPO
+ORG=$ORG
 ACCESS_TOKEN=$TOKEN
-REG_TOKEN=$REG_TOKEN
+NAME=$NAME
 
-# REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$(curl -X POST -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/orgs/${ORG}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --unattended --url https://github.com/${REPO} --token ${REG_TOKEN} --name TEST
+./config.sh --unattended --url https://github.com/${REPO} --token ${REG_TOKEN} --name ${NAME} --replace
 
 cleanup() {
 echo "Removing runner..."
