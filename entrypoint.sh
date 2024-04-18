70ee924bb1a04eba284bafe43778aee3e24f5538
Chesney Nyffels <chesney@nyffels.be>
2024-04-18 12:16:00 +0200

    Updated entryp[oint with commentary

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+# Environment variables
 ORG=$ORG
 PAT=$PAT
 NAME=$NAME
-ACTIONS_RUNNER_INPUT_REPLACE=true
 
+# Script variables
+ACTIONS_RUNNER_INPUT_REPLACE=true
+RUNNER_ALLOW_RUNASROOT=true
+
+# Fetch the runner registration token with a personal access token
 REG_TOKEN=$(curl -X POST -H "Authorization: Bearer ${PAT}" -H "Accept: application/vnd.github+json" https://api.github.com/orgs/${ORG}/actions/runners/registration-token | jq .token --raw-output)
 
+# Run the script to create the runner
 cd /actions-runner
-
 echo "Runner = ${$NAME}"
-RUNNER_ALLOW_RUNASROOT=true ./config.sh --unattended --url "https://github.com/${ORG}" --token $REG_TOKEN --name $NAME
+./config.sh --unattended --url "https://github.com/${ORG}" --token $REG_TOKEN --name $NAME
 
+# Remove the runner on container end-of-live
 cleanup() {
 echo "Removing runner..."
     ./config.sh remove --token $REG_TOKEN
@@ -21,5 +27,6 @@ trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 trap 'cleanup' SIGTERM
 
+# Start the runner
 echo "Starting runner..."
 ./run.sh & wait $!
