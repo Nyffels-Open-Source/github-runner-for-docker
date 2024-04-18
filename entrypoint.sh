ecb82c359e5b3665054d76f7bc9285be424ab241
Chesney Nyffels <chesney@nyffels.be>
2024-04-18 12:32:02 +0200

    Updated entrypoint config file

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,14 @@ NAME=$NAME
 
 # Script variables
 ACTIONS_RUNNER_INPUT_REPLACE=true
-RUNNER_ALLOW_RUNASROOT=true
+RUNNER_ALLOW_RUNASROOT="true"
 
 # Fetch the runner registration token with a personal access token
 REG_TOKEN=$(curl -X POST -H "Authorization: Bearer ${PAT}" -H "Accept: application/vnd.github+json" https://api.github.com/orgs/${ORG}/actions/runners/registration-token | jq .token --raw-output)
 
 # Run the script to create the runner
 cd /actions-runner
-RUNNER_ALLOW_RUNASROOT=true ./config.sh --unattended --url "https://github.com/${ORG}" --token $REG_TOKEN --name $NAME
+./config.sh --unattended --url "https://github.com/${ORG}" --token $REG_TOKEN --name $NAME
 
 # Remove the runner on container end-of-live
 cleanup() {
